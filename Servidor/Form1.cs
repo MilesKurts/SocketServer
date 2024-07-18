3c14ccaf5d9045e43a550c46be1bc5009c1aa9d2
MilesKurts <milesheremau@gmail.com>
2024-07-18 11:54:39 -0300

    new

--- Servidor/Form1.cs
+++ Servidor/Form1.cs
@@ -10,31 +10,74 @@ using System.Windows.Forms;
 
 namespace Servidor
 {
+    
     public partial class Form1 : Form
     {
+        Task tarefa;
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            txtPasta.Text = FTServer.PastaRecepcaoArquivos;
+            FTServer.ListaMensagem = listaLogs;
 
         }
 
         private void btnConexao_Click(object sender, EventArgs e)
         {
+            int porta = (int)txtPorta.Value;
+            string endIP = txtEnderecoIP.Text;
+
+            try
+            {
+                FTServer.EnderecoIP = endIP;
+                FTServer.PortaHost = porta;
+
+                tarefa = Task.Factory.StartNew(() =>
+                {
+                    FTServer.iniciarServidor();
+                });
+            }
+            catch (Exception ex)
+            {
+                listaLogs.Invoke(new Action(() =>
+                {
+                    listaLogs.Items.Add($"Erro ao conectar: {ex.Message}");
+                    listaLogs.SetSelected(listaLogs.Items.Count - 1, true);
+                }));
+            }
 
         }
 
         private void btnPararServidor_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                Application.Restart();
+            }
+            catch(Exception ex)
+            {
+                listaLogs.Invoke(new Action(() =>
+                {
+                    listaLogs.Items.Add($"Erro : {ex.Message}");
+                    listaLogs.SetSelected(listaLogs.Items.Count - 1, true);
+                }));
+            }
         }
 
         private void txtPasta_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() != DialogResult.Cancel)
+            {
+                FTServer.PastaRecepcaoArquivos = dialog.SelectedPath +@"\";
+                txtPasta.Text = FTServer.PastaRecepcaoArquivos;
 
+            }
         }
     }
 }
